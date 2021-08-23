5c853a44ecce1af49a5894617aab8a298934a961
Brooks Townsend <brooks.townsend@capitalone.com>
2021-08-23 10:40:58 -0400

    initial commit for working stable trace allocator

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# trallocator
+# stable trallocator
 Custom allocator to debug heap allocations as introduced by this comment: https://www.reddit.com/r/rust/comments/8z83wc/is_there_any_way_to_benchmark_memory_usage_in_rust/
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,51 @@
-use std::alloc::{GlobalAlloc, Layout};
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::sync::atomic::{AtomicU64, Ordering::SeqCst};
+pub struct Strallocator;
 
-pub struct Trallocator<A: GlobalAlloc>(pub A, AtomicU64);
+static ALLOCATED: AtomicU64 = AtomicU64::new(0);
 
-unsafe impl<A: GlobalAlloc> GlobalAlloc for Trallocator<A> {
+unsafe impl GlobalAlloc for Strallocator {
     unsafe fn alloc(&self, l: Layout) -> *mut u8 {
-        self.1.fetch_add(l.size() as u64, Ordering::SeqCst);
-        self.0.alloc(l)
+        ALLOCATED.fetch_add(l.size() as u64, SeqCst);
+        System.alloc(l)
     }
+
     unsafe fn dealloc(&self, ptr: *mut u8, l: Layout) {
-        self.0.dealloc(ptr, l);
-        self.1.fetch_sub(l.size() as u64, Ordering::SeqCst);
+        ALLOCATED.fetch_sub(l.size() as u64, SeqCst);
+        System.dealloc(ptr, l);
     }
 }
 
-impl<A: GlobalAlloc> Trallocator<A> {
-    pub fn new(a: A) -> Self {
-        Trallocator(a, AtomicU64::new(0))
-    }
-
+impl Strallocator {
     pub fn reset(&self) {
-        self.1.store(0, Ordering::SeqCst);
+        ALLOCATED.store(0, SeqCst);
     }
+
     pub fn get(&self) -> u64 {
-        self.1.load(Ordering::SeqCst)
+        ALLOCATED.load(SeqCst)
     }
 }
+
+// unsafe impl<A: GlobalAlloc> GlobalAlloc for Trallocator<A> {
+//     unsafe fn alloc(&self, l: Layout) -> *mut u8 {
+//         self.1.fetch_add(l.size() as u64, Ordering::SeqCst);
+//         self.0.alloc(l)
+//     }
+//     unsafe fn dealloc(&self, ptr: *mut u8, l: Layout) {
+//         self.0.dealloc(ptr, l);
+//         self.1.fetch_sub(l.size() as u64, Ordering::SeqCst);
+//     }
+// }
+
+// impl<A: GlobalAlloc> Trallocator<A> {
+//     pub fn new(a: A) -> Self {
+//         Trallocator(a, AtomicU64::new(0))
+//     }
+
+//     pub fn reset(&self) {
+//         self.1.store(0, Ordering::SeqCst);
+//     }
+//     pub fn get(&self) -> u64 {
+//         self.1.load(Ordering::SeqCst)
+//     }
+// }
