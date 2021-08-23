8a53c8a7fe69fc13f10c026886f0e0e345915ed9
Brooks Townsend <brooks.townsend@capitalone.com>
2021-08-23 10:41:14 -0400

    removed trallocator artifacts

--- src/lib.rs
+++ src/lib.rs
@@ -25,27 +25,3 @@ impl Strallocator {
         ALLOCATED.load(SeqCst)
     }
 }
-
-// unsafe impl<A: GlobalAlloc> GlobalAlloc for Trallocator<A> {
-//     unsafe fn alloc(&self, l: Layout) -> *mut u8 {
-//         self.1.fetch_add(l.size() as u64, Ordering::SeqCst);
-//         self.0.alloc(l)
-//     }
-//     unsafe fn dealloc(&self, ptr: *mut u8, l: Layout) {
-//         self.0.dealloc(ptr, l);
-//         self.1.fetch_sub(l.size() as u64, Ordering::SeqCst);
-//     }
-// }
-
-// impl<A: GlobalAlloc> Trallocator<A> {
-//     pub fn new(a: A) -> Self {
-//         Trallocator(a, AtomicU64::new(0))
-//     }
-
-//     pub fn reset(&self) {
-//         self.1.store(0, Ordering::SeqCst);
-//     }
-//     pub fn get(&self) -> u64 {
-//         self.1.load(Ordering::SeqCst)
-//     }
-// }
